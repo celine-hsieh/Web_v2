9738e0104794771451a6c4f2cf1e33e14e1587a1
celinehsieh <kingspade880608@gmail.com>
2023-02-06 14:48:24 +0800

    0206_v8

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   fs.readFile('./squit_game/models/index.html', (err, data) => {

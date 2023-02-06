72154e0ead512d307900078140b515614ff8bb8b
celinehsieh <kingspade880608@gmail.com>
2023-02-06 14:26:25 +0800

    0206_v7

--- server.js
+++ server.js
@@ -20,4 +20,4 @@ const server = http.createServer((req, res) => {
 // });
 
 server.listen(port, () => { console.log(`Server listening on port ${port}`); });
-module.exports = app
+//module.exports = app

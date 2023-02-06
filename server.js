b5d51c175672ec58c128f0fca8ee030983be921c
celinehsieh <kingspade880608@gmail.com>
2023-02-06 14:05:14 +0800

    0206_v3

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-  fs.readFile('index.html', (err, data) => {
+  fs.readFile('./squit_game/index.html', (err, data) => {
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
       return res.end("404 Not Found");
